ed329815d0f2b802fd9ee2d0007db6400fa03b2a
AdinAck <adinackerman@gmail.com>
2021-12-16 11:32:09 -0800

    Initial Commit

---
+++ Polygon/PolygonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolygonApp.swift
+//  Polygon
+//
+//  Created by Adin Ackerman on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct PolygonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
